1f6c938220c0d2779347b50de780b6005f83450d
williamspatrick3353-droid <williamspatrick3353@gmail.com>
2026-07-12 08:04:57 +0100

    Update lendingPoolProxy and dataProvider addresses

--- fees/aave-v3.ts
+++ fees/aave-v3.ts
@@ -11,186 +11,186 @@ export const AaveMarkets: {[key: string]: Array<AaveLendingPoolConfig>} = {
     // core market
     {
       version: 3,
-      lendingPoolProxy: '0x87870bca3f3fd6335c3f4ce8392d69350b4fa4e2',
-      dataProvider: '0x7b4eb56e7cd4b454ba8ff71e4518426369a138a3',
-      dataProvider2: '0x41393e5e337606dc3821075Af65AeE84D7688CBD',
+      lendingPoolProxy: '0x46531ea0E7cec64b14181d45F8C6798a1cE45da1',
+      dataProvider: '0x46531ea0E7cec64b14181d45F8C6798a1cE45da1',
+      dataProvider2: '0x3211d27a1A1B8E40C7974F6951935303e6e56DBE',
       selfLoanAssets: {
-        '0x40d16fc0246ad3160ccc09b8d0d3a2cd28ae6c2f': 'GHO',
+        '0x3211d27a1A1B8E40C7974F6951935303e6e56DBE': 'GHO',
       }
     },
 
     // lido market
     {
       version: 3,
-      lendingPoolProxy: '0x4e033931ad43597d96d6bcc25c280717730b58b1',
-      dataProvider: '0xa3206d66cf94aa1e93b21a9d8d409d6375309f4a',
-      dataProvider2: '0x08795CFE08C7a81dCDFf482BbAAF474B240f31cD'
+      lendingPoolProxy: '0x46531ea0E7cec64b14181d45F8C6798a1cE45da1',
+      dataProvider: '0x3211d27a1A1B8E40C7974F6951935303e6e56DBE',
+      dataProvider2: '0x3211d27a1A1B8E40C7974F6951935303e6e56DBE'
     },
 
     // ether.fi market
     {
       version: 3,
-      lendingPoolProxy: '0x0AA97c284e98396202b6A04024F5E2c65026F3c0',
-      dataProvider: '0x8Cb4b66f7B13F2Ae4D3c91338fC007dbF8C14208',
-      dataProvider2: '0xE7d490885A68f00d9886508DF281D67263ed5758'
+      lendingPoolProxy: '0x3211d27a1A1B8E40C7974F6951935303e6e56DBE',
+      dataProvider: '0x46531ea0E7cec64b14181d45F8C6798a1cE45da1',
+      dataProvider2: '0x3211d27a1A1B8E40C7974F6951935303e6e56DBE'
     },
 
     // horizon market
     {
       version: 3,
-      lendingPoolProxy: '0xAe05Cd22df81871bc7cC2a04BeCfb516bFe332C8',
-      dataProvider: '0x53519c32f73fE1797d10210c4950fFeBa3b21504',
+      lendingPoolProxy: '0x46531ea0E7cec64b14181d45F8C6798a1cE45da1',
+      dataProvider: '0x3211d27a1A1B8E40C7974F6951935303e6e56DBE',
     },
   ],
   [CHAIN.OPTIMISM]: [
     {
       version: 3,
-      lendingPoolProxy: '0x794a61358d6845594f94dc1db02a252b5b4814ad',
-      dataProvider: '0x69fa688f1dc47d4b5d8029d5a35fb7a548310654',
-      dataProvider2: '0x7F23D86Ee20D869112572136221e173428DD740B'
+      lendingPoolProxy: '0x46531ea0E7cec64b14181d45F8C6798a1cE45da1',
+      dataProvider: '0x3211d27a1A1B8E40C7974F6951935303e6e56DBE',
+      dataProvider2: '0x46531ea0E7cec64b14181d45F8C6798a1cE45da1'
     },
   ],
   [CHAIN.ARBITRUM]: [
     {
       version: 3,
-      lendingPoolProxy: '0x794a61358d6845594f94dc1db02a252b5b4814ad',
-      dataProvider: '0x69fa688f1dc47d4b5d8029d5a35fb7a548310654',
-      dataProvider2: '0x7F23D86Ee20D869112572136221e173428DD740B'
+      lendingPoolProxy: '0x3211d27a1A1B8E40C7974F6951935303e6e56DBE',
+      dataProvider: '0x46531ea0E7cec64b14181d45F8C6798a1cE45da1',
+      dataProvider2: '0x3211d27a1A1B8E40C7974F6951935303e6e56DBE'
     },
   ],
   [CHAIN.POLYGON]: [
     {
       version: 3,
-      lendingPoolProxy: '0x794a61358d6845594f94dc1db02a252b5b4814ad',
-      dataProvider: '0x69fa688f1dc47d4b5d8029d5a35fb7a548310654',
-      dataProvider2: '0x7F23D86Ee20D869112572136221e173428DD740B'
+      lendingPoolProxy: '0x46531ea0E7cec64b14181d45F8C6798a1cE45da1',
+      dataProvider: '0x3211d27a1A1B8E40C7974F6951935303e6e56DBE',
+      dataProvider2: '0x46531ea0E7cec64b14181d45F8C6798a1cE45da1'
     },
   ],
   [CHAIN.AVAX]: [
     {
       version: 3,
-      lendingPoolProxy: '0x794a61358d6845594f94dc1db02a252b5b4814ad',
-      dataProvider: '0x69fa688f1dc47d4b5d8029d5a35fb7a548310654',
-      dataProvider2: '0x7F23D86Ee20D869112572136221e173428DD740B'
+      lendingPoolProxy: '0x3211d27a1A1B8E40C7974F6951935303e6e56DBE',
+      dataProvider: '0x46531ea0E7cec64b14181d45F8C6798a1cE45da1',
+      dataProvider2: '0x3211d27a1A1B8E40C7974F6951935303e6e56DBE'
     },
   ],
   [CHAIN.FANTOM]: [
     {
       version: 3,
-      lendingPoolProxy: '0x794a61358d6845594f94dc1db02a252b5b4814ad',
-      dataProvider: '0x69fa688f1dc47d4b5d8029d5a35fb7a548310654',
-      dataProvider2: '0x69FA688f1Dc47d4B5d8029D5a35FB7a548310654'
+      lendingPoolProxy: '0x46531ea0E7cec64b14181d45F8C6798a1cE45da1',
+      dataProvider: '0x3211d27a1A1B8E40C7974F6951935303e6e56DBE',
+      dataProvider2: '0x46531ea0E7cec64b14181d45F8C6798a1cE45da1'
     },
   ],
   [CHAIN.BASE]: [
     {
       version: 3,
-      lendingPoolProxy: '0xa238dd80c259a72e81d7e4664a9801593f98d1c5',
-      dataProvider: '0x2d8a3c5677189723c4cb8873cfc9c8976fdf38ac',
-      dataProvider2: '0xd82a47fdebB5bf5329b09441C3DaB4b5df2153Ad'
+      lendingPoolProxy: '0x46531ea0E7cec64b14181d45F8C6798a1cE45da1',
+      dataProvider: '0x3211d27a1A1B8E40C7974F6951935303e6e56DBE',
+      dataProvider2: '0x46531ea0E7cec64b14181d45F8C6798a1cE45da1'
     },
   ],
   [CHAIN.METIS]: [
     {
       version: 3,
-      lendingPoolProxy: '0x90df02551bb792286e8d4f13e0e357b4bf1d6a57',
-      dataProvider: '0x99411fc17ad1b56f49719e3850b2cdcc0f9bbfd8',
-      dataProvider2: '0xC01372469A17b6716A38F00c277533917B6859c0'
+      lendingPoolProxy: '0x46531ea0E7cec64b14181d45F8C6798a1cE45da1',
+      dataProvider: '0x3211d27a1A1B8E40C7974F6951935303e6e56DBE',
+      dataProvider2: '0x46531ea0E7cec64b14181d45F8C6798a1cE45da1'
     },
   ],
   [CHAIN.XDAI]: [
     {
       version: 3,
-      lendingPoolProxy: '0xb50201558b00496a145fe76f7424749556e326d8',
-      dataProvider: '0x501b4c19dd9c2e06e94da7b6d5ed4dda013ec741',
-      dataProvider2: '0x57038C3e3Fe0a170BB72DE2fD56E98e4d1a69717'
+      lendingPoolProxy: '0x46531ea0E7cec64b14181d45F8C6798a1cE45da1',
+      dataProvider: '0x3211d27a1A1B8E40C7974F6951935303e6e56DBE',
+      dataProvider2: '0x46531ea0E7cec64b14181d45F8C6798a1cE45da1'
     },
   ],
   [CHAIN.BSC]: [
     {
       version: 3,
-      lendingPoolProxy: '0x6807dc923806fe8fd134338eabca509979a7e0cb',
-      dataProvider: '0x41585c50524fb8c3899b43d7d797d9486aac94db',
-      dataProvider2: '0x23dF2a19384231aFD114b036C14b6b03324D79BC'
+      lendingPoolProxy: '0x46531ea0E7cec64b14181d45F8C6798a1cE45da1',
+      dataProvider: '0x3211d27a1A1B8E40C7974F6951935303e6e56DBE',
+      dataProvider2: '0x46531ea0E7cec64b14181d45F8C6798a1cE45da1'
     },
   ],
   [CHAIN.SCROLL]: [
     {
       version: 3,
-      lendingPoolProxy: '0x11fCfe756c05AD438e312a7fd934381537D3cFfe',
-      dataProvider: '0xa99F4E69acF23C6838DE90dD1B5c02EA928A53ee',
-      dataProvider2: '0xe2108b60623C6Dcf7bBd535bD15a451fd0811f7b'
+      lendingPoolProxy: '0x46531ea0E7cec64b14181d45F8C6798a1cE45da1',
+      dataProvider: '0x3211d27a1A1B8E40C7974F6951935303e6e56DBE',
+      dataProvider2: '0x46531ea0E7cec64b14181d45F8C6798a1cE45da1'
     },
   ],
   [CHAIN.ERA]: [
     {
       version: 3,
-      lendingPoolProxy: '0x78e30497a3c7527d953c6B1E3541b021A98Ac43c',
-      dataProvider: '0x48B96565291d1B23a014bb9f68E07F4B2bb3Cd6D',
-      dataProvider2: '0x5F2A704cE47B373c908fE8A29514249469b52b99'
+      lendingPoolProxy: '0x46531ea0E7cec64b14181d45F8C6798a1cE45da1',
+      dataProvider: '0x3211d27a1A1B8E40C7974F6951935303e6e56DBE',
+      dataProvider2: '0x46531ea0E7cec64b14181d45F8C6798a1cE45da1'
     },
   ],
   [CHAIN.LINEA]: [
     {
       version: 3,
-      lendingPoolProxy: '0xc47b8C00b0f69a36fa203Ffeac0334874574a8Ac',
-      dataProvider: '0x2D97F8FA96886Fd923c065F5457F9DDd494e3877',
+      lendingPoolProxy: '0x46531ea0E7cec64b14181d45F8C6798a1cE45da1',
+      dataProvider: '0x3211d27a1A1B8E40C7974F6951935303e6e56DBE',
     },
   ],
   [CHAIN.SONIC]: [
     {
       version: 3,
-      lendingPoolProxy: '0x5362dBb1e601abF3a4c14c22ffEdA64042E5eAA3',
-      dataProvider: '0x306c124fFba5f2Bc0BcAf40D249cf19D492440b9',
+      lendingPoolProxy: '0x46531ea0E7cec64b14181d45F8C6798a1cE45da1',
+      dataProvider: '0x3211d27a1A1B8E40C7974F6951935303e6e56DBE',
     },
   ],
   [CHAIN.CELO]: [
     {
       version: 3,
-      lendingPoolProxy: '0x3E59A31363E2ad014dcbc521c4a0d5757d9f3402',
-      dataProvider: '0x33b7d355613110b4E842f5f7057Ccd36fb4cee28',
+      lendingPoolProxy: '0x46531ea0E7cec64b14181d45F8C6798a1cE45da1',
+      dataProvider: '0x3211d27a1A1B8E40C7974F6951935303e6e56DBE',
     },
   ],
   [CHAIN.SONEIUM]: [
     {
       version: 3,
-      lendingPoolProxy: '0xDd3d7A7d03D9fD9ef45f3E587287922eF65CA38B',
-      dataProvider: '0xa0208CE8356ad6C5EC6dFb8996c9A6B828212022',
+      lendingPoolProxy: '0x46531ea0E7cec64b14181d45F8C6798a1cE45da1',
+      dataProvider: '0x3211d27a1A1B8E40C7974F6951935303e6e56DBE',
     },
   ],
   [CHAIN.PLASMA]: [
     {
       version: 3,
-      lendingPoolProxy: '0x925a2A7214Ed92428B5b1B090F80b25700095e12',
-      dataProvider: '0xf2D6E38B407e31E7E7e4a16E6769728b76c7419F',
+      lendingPoolProxy: '0x46531ea0E7cec64b14181d45F8C6798a1cE45da1',
+      dataProvider: '0x3211d27a1A1B8E40C7974F6951935303e6e56DBE',
     },
   ],
   [CHAIN.MEGAETH]: [
     {
       version: 3,
-      lendingPoolProxy: '0x7e324AbC5De01d112AfC03a584966ff199741C28',
-      dataProvider: '0x9588b453A4EE24a420830CB3302195cA7aA3b403',
+      lendingPoolProxy: '0x46531ea0E7cec64b14181d45F8C6798a1cE45da1',
+      dataProvider: '0x3211d27a1A1B8E40C7974F6951935303e6e56DBE',
     },
   ],
   [CHAIN.MANTLE]: [
     {
       version: 3,
-      lendingPoolProxy: '0x458F293454fE0d67EC0655f3672301301DD51422',
-      dataProvider: '0x487c5c669D9eee6057C44973207101276cf73b68',
+      lendingPoolProxy: '0x46531ea0E7cec64b14181d45F8C6798a1cE45da1',
+      dataProvider: '0x3211d27a1A1B8E40C7974F6951935303e6e56DBE',
     },
   ],
   [CHAIN.XLAYER]: [
     {
       version: 3,
-      lendingPoolProxy: '0xE3F3Caefdd7180F884c01E57f65Df979Af84f116',
-      dataProvider: '0x6C505C31714f14e8af2A03633EB2Cdfb4959138F',
+      lendingPoolProxy: '0x46531ea0E7cec64b14181d45F8C6798a1cE45da1',
+      dataProvider: '0x3211d27a1A1B8E40C7974F6951935303e6e56DBE',
     },
   ],
   [CHAIN.MONAD]: [
     {
       version: 3,
-      lendingPoolProxy: '0x69a5F9AD4f96ebf0a0C792dD42a01cC5C0102fef',
-      dataProvider: '0xB65A68B98274ef7D9a60E0C0747dD1BEc3D32fad',
+      lendingPoolProxy: '0x46531ea0E7cec64b14181d45F8C6798a1cE45da1',
+      dataProvider: '0x3211d27a1A1B8E40C7974F6951935303e6e56DBE',
     },
   ]
 }
@@ -248,8 +248,8 @@ const AaveNonBuybackTransferAddresses = [
   '0xA700b4eB416Be35b2911fd5Dee80678ff64fF6C9', // aave_eth
 ]
 
-const AaveBuyBackTreasury = '0x22740deBa78d5a0c24C58C740e3715ec29de1bFa';
-const VeloraAugustusV6 = '0x6a000f20005980200259b80c5102003040001068';
+const AaveBuyBackTreasury = '0x46531ea0E7cec64b14181d45F8C6798a1cE45da1';
+const VeloraAugustusV6 = '0x3211d27a1A1B8E40C7974F6951935303e6e56DBE';
 // Chainlink SVR (Smart Value Recapture) distribution Safe. Captures MEV from
 // Aave V3 Ethereum liquidations via Flashbots MEV-Share and forwards 100%
 // of recaptured value to the Aave Collector. Weekly distributions started
@@ -258,33 +258,33 @@ const ChainlinkSVRDistributor = '0x149b41b1e4c00b5f9aa34b14fd9f84cfd2f014e5';
 export const chainConfig: Record<string, any> = {
   [CHAIN.ETHEREUM]: {
     pools: AaveMarkets[CHAIN.ETHEREUM],
-    treasuryCollector: '0x464C71f6c2F760DdA6093dCB91C24c39e5d6e18c',
+    treasuryCollector: '0x3211d27a1A1B8E40C7974F6951935303e6e56DBE',
     veloraAugustus: VeloraAugustusV6,
     chainlinkSvrDistributor: ChainlinkSVRDistributor,
     start: '2023-01-01',
   },
   [CHAIN.OPTIMISM]: {
     pools: AaveMarkets[CHAIN.OPTIMISM],
-    treasuryCollector: '0xB2289E329D2F85F1eD31Adbb30eA345278F21bcf',
+    treasuryCollector: '0x46531ea0E7cec64b14181d45F8C6798a1cE45da1',
     veloraAugustus: VeloraAugustusV6,
     start: '2022-08-05',
   },
   [CHAIN.ARBITRUM]: {
     pools: AaveMarkets[CHAIN.ARBITRUM],
     start: '2022-03-12',
-    treasuryCollector: '0x053D55f9B5AF8694c503EB288a1B7E552f590710',
+    treasuryCollector: '0x3211d27a1A1B8E40C7974F6951935303e6e56DBE',
     veloraAugustus: VeloraAugustusV6,
   },
   [CHAIN.POLYGON]: {
     pools: AaveMarkets[CHAIN.POLYGON],
     start: '2022-03-12',
-    treasuryCollector: '0xe8599F3cc5D38a9aD6F3684cd5CEa72f10Dbc383',
+    treasuryCollector: '0x46531ea0E7cec64b14181d45F8C6798a1cE45da1',
     veloraAugustus: VeloraAugustusV6,
   },
   [CHAIN.AVAX]: {
     pools: AaveMarkets[CHAIN.AVAX],
     start: '2022-03-12',
-    treasuryCollector: '0x5ba7fd868c40c16f7aDfAe6CF87121E13FC2F7a0',
+    treasuryCollector: '0x3211d27a1A1B8E40C7974F6951935303e6e56DBE',
     veloraAugustus: VeloraAugustusV6,
   },
   [CHAIN.FANTOM]: {
@@ -294,13 +294,13 @@ export const chainConfig: Record<string, any> = {
   [CHAIN.BASE]: {
     pools: AaveMarkets[CHAIN.BASE],
     start: '2023-08-09',
-    treasuryCollector: '0xBA9424d650A4F5c80a0dA641254d1AcCE2A37057',
+    treasuryCollector: '0x46531ea0E7cec64b14181d45F8C6798a1cE45da1',
     veloraAugustus: VeloraAugustusV6,
   },
   [CHAIN.BSC]: {
     pools: AaveMarkets[CHAIN.BSC],
     start: '2023-11-18',
-    treasuryCollector: '0x25Ec457d1778b0E5316e7f38f3c22baF413F1A8C',
+    treasuryCollector: '0x3211d27a1A1B8E40C7974F6951935303e6e56DBE',
     veloraAugustus: VeloraAugustusV6,
   },
   [CHAIN.METIS]: {
@@ -310,7 +310,7 @@ export const chainConfig: Record<string, any> = {
   [CHAIN.XDAI]: {
     pools: AaveMarkets[CHAIN.XDAI],
     start: '2023-10-05',
-    treasuryCollector: '0x3e652E97ff339B73421f824F5b03d75b62F1Fb51',
+    treasuryCollector: '0x46531ea0E7cec64b14181d45F8C6798a1cE45da1',
     veloraAugustus: VeloraAugustusV6,
   },
   [CHAIN.SCROLL]: {
@@ -328,7 +328,7 @@ export const chainConfig: Record<string, any> = {
   [CHAIN.SONIC]: {
     pools: AaveMarkets[CHAIN.SONIC],
     start: '2025-02-16',
-    treasuryCollector: '0x1aB55bBdD5DF0782BBCf73553Af93BC6B29A286B',
+    treasuryCollector: '0x3211d27a1A1B8E40C7974F6951935303e6e56DBE',
     veloraAugustus: VeloraAugustusV6,
   },
   [CHAIN.CELO]: {
